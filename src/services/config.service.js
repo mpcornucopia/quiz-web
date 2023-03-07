cac367e7cb7a779bacb7aa940ceb5067b3674c93
mpcornucopia <matthewtjpotts@live.co.uk>
2023-03-07 11:51:32 +0000

    changed backend url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapimp-env.eba-2zmp26zy.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
